eec2071bc683e80625407ffb2770d1c7d0c1e38d
Alban Diguer <alban.diguer@gmail.com>
2025-11-03 18:58:06 +0100

    fix: reverse proxy for plane

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Usage: ./restart.sh <service-name>
-ssh hetzner "sudo -u podman XDG_RUNTIME_DIR=/run/user/\$(id -u podman) systemctl --user restart $1.service && sudo -u podman XDG_RUNTIME_DIR=/run/user/\$(id -u podman) journalctl --user -u $1.service -f"
+ssh hetzner "sudo -u podman XDG_RUNTIME_DIR=/run/user/\$(id -u podman) systemctl --user restart $1.service && sudo -u podman XDG_RUNTIME_DIR=/run/user/\$(id -u podman) journalctl --user -u $1.service -n 20"
