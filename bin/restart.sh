248c4436b4561b357738d38b620b7b2a132b885d
Alban Diguer <alban.diguer@gmail.com>
2026-04-21 21:40:36 +0200

    Fix restart.sh to map plane -> plane-pod.service

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Usage: ./restart.sh <service-name>
+# Maps directory names to actual systemd service names (e.g., plane -> plane-pod)
 
 SERVICE=${1:-$SERVICE}
 
@@ -9,4 +10,10 @@ if [ -z "$SERVICE" ]; then
 	exit 1
 fi
 
-ssh minilab "sudo -u podman XDG_RUNTIME_DIR=/run/user/\$(id -u podman) systemctl --user restart ${SERVICE}.service && sudo -u podman XDG_RUNTIME_DIR=/run/user/\$(id -u podman) journalctl --user -u ${SERVICE}.service -n 20"
+# Map directory names to actual systemd service names
+case "$SERVICE" in
+	plane) SERVICE_UNIT="plane-pod" ;;
+	*) SERVICE_UNIT="$SERVICE" ;;
+esac
+
+ssh minilab "sudo -u podman XDG_RUNTIME_DIR=/run/user/\$(id -u podman) systemctl --user restart ${SERVICE_UNIT}.service && sudo -u podman XDG_RUNTIME_DIR=/run/user/\$(id -u podman) journalctl --user -u ${SERVICE_UNIT}.service -n 20"
